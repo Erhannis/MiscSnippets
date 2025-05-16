269d374cf6c0733b05fe8edc7189fc6ec8e6a6af
erhannis <eyeillus@gmail.com>
2025-05-16 12:11:29 -0400

    Added source for bookmarklets

--- bookmarklets/jlcpcb_copy.js
+++ bookmarklets/jlcpcb_copy.js
@@ -1,1 +1,29 @@
-javascript:a1%20%3D%20document.evaluate%28%22%2F%2Fdl%5Bcontains%28.%2C%20%27MFR.Part%20%23%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0Aa2%20%3D%20a1.iterateNext%28%29%3B%0A%0Aif%20%28a2%20%3D%3D%20null%29%20%7B%0A%20%20a1%20%3D%20document.evaluate%28%22%2F%2Ftr%5Bcontains%28.%2C%20%27Mfr.%20Part%20%23%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0A%20%20a2%20%3D%20a1.iterateNext%28%29%3B%0A%7D%0A%0Ab1%20%3D%20document.evaluate%28%22%2F%2Fdl%5Bcontains%28.%2C%20%27JLCPCB%20Part%20%23%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0Ab2%20%3D%20b1.iterateNext%28%29%3B%0A%0Aif%20%28b2%20%3D%3D%20null%29%20%7B%0A%20%20b1%20%3D%20document.evaluate%28%22%2F%2Ftr%5Bcontains%28.%2C%20%27LCSC%20Part%20%23%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0A%20%20b2%20%3D%20b1.iterateNext%28%29%3B%0A%7D%0A%0Ac1%20%3D%20document.evaluate%28%22%2F%2Fdl%5Bcontains%28.%2C%20%27Description%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0Ac2%20%3D%20c1.iterateNext%28%29%3B%0A%0Aif%20%28c2%20%3D%3D%20null%29%20%7B%0A%20%20c1%20%3D%20document.evaluate%28%22%2F%2Ftr%5Bcontains%28.%2C%20%27Description%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0A%20%20c2%20%3D%20c1.iterateNext%28%29%3B%0A%7D%0A%0Anavigator.clipboard.writeText%28%22Description%5Ct%22%2Bc2.childNodes%5B2%5D.textContent.trim%28%29%2B%22%5Cn%22%2B%22MFR%5Ct%22%2Ba2.childNodes%5B2%5D.textContent.trim%28%29%2B%22%5Cn%22%2B%22LCSC%5Ct%22%2Bb2.childNodes%5B2%5D.textContent.trim%28%29%2B%22%5Cn%22%2B%22URL%5Ct%22%2Bwindow.location%29%3B
+// javascript:a1%20%3D%20document.evaluate%28%22%2F%2Fdl%5Bcontains%28.%2C%20%27MFR.Part%20%23%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0Aa2%20%3D%20a1.iterateNext%28%29%3B%0A%0Aif%20%28a2%20%3D%3D%20null%29%20%7B%0A%20%20a1%20%3D%20document.evaluate%28%22%2F%2Ftr%5Bcontains%28.%2C%20%27Mfr.%20Part%20%23%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0A%20%20a2%20%3D%20a1.iterateNext%28%29%3B%0A%7D%0A%0Ab1%20%3D%20document.evaluate%28%22%2F%2Fdl%5Bcontains%28.%2C%20%27JLCPCB%20Part%20%23%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0Ab2%20%3D%20b1.iterateNext%28%29%3B%0A%0Aif%20%28b2%20%3D%3D%20null%29%20%7B%0A%20%20b1%20%3D%20document.evaluate%28%22%2F%2Ftr%5Bcontains%28.%2C%20%27LCSC%20Part%20%23%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0A%20%20b2%20%3D%20b1.iterateNext%28%29%3B%0A%7D%0A%0Ac1%20%3D%20document.evaluate%28%22%2F%2Fdl%5Bcontains%28.%2C%20%27Description%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0Ac2%20%3D%20c1.iterateNext%28%29%3B%0A%0Aif%20%28c2%20%3D%3D%20null%29%20%7B%0A%20%20c1%20%3D%20document.evaluate%28%22%2F%2Ftr%5Bcontains%28.%2C%20%27Description%27%29%5D%22%2C%20document%2C%20null%2C%20XPathResult.ANY_TYPE%2C%20null%20%29%3B%0A%20%20c2%20%3D%20c1.iterateNext%28%29%3B%0A%7D%0A%0Anavigator.clipboard.writeText%28%22Description%5Ct%22%2Bc2.childNodes%5B2%5D.textContent.trim%28%29%2B%22%5Cn%22%2B%22MFR%5Ct%22%2Ba2.childNodes%5B2%5D.textContent.trim%28%29%2B%22%5Cn%22%2B%22LCSC%5Ct%22%2Bb2.childNodes%5B2%5D.textContent.trim%28%29%2B%22%5Cn%22%2B%22URL%5Ct%22%2Bwindow.location%29%3B
+
+
+
+a1 = document.evaluate("//dl[contains(., 'MFR.Part #')]", document, null, XPathResult.ANY_TYPE, null );
+a2 = a1.iterateNext();
+
+if (a2 == null) {
+  a1 = document.evaluate("//tr[contains(., 'Mfr. Part #')]", document, null, XPathResult.ANY_TYPE, null );
+  a2 = a1.iterateNext();
+}
+
+b1 = document.evaluate("//dl[contains(., 'JLCPCB Part #')]", document, null, XPathResult.ANY_TYPE, null );
+b2 = b1.iterateNext();
+
+if (b2 == null) {
+  b1 = document.evaluate("//tr[contains(., 'LCSC Part #')]", document, null, XPathResult.ANY_TYPE, null );
+  b2 = b1.iterateNext();
+}
+
+c1 = document.evaluate("//dl[contains(., 'Description')]", document, null, XPathResult.ANY_TYPE, null );
+c2 = c1.iterateNext();
+
+if (c2 == null) {
+  c1 = document.evaluate("//tr[contains(., 'Description')]", document, null, XPathResult.ANY_TYPE, null );
+  c2 = c1.iterateNext();
+}
+
+navigator.clipboard.writeText("Description\t"+c2.childNodes[2].textContent.trim()+"\n"+"MFR\t"+a2.childNodes[2].textContent.trim()+"\n"+"LCSC\t"+b2.childNodes[2].textContent.trim()+"\n"+"URL\t"+window.location);
--- bookmarklets/track_dhl.js
+++ bookmarklets/track_dhl.js
@@ -1,1 +1,8 @@
-javascript:tracking%20%3D%20prompt%28%22Enter%20DHL%20tracking%20number%22%29%3B%0Aurl%20%3D%20%22https%3A%2F%2Fwww.dhl.com%2Fus-en%2Fhome%2Ftracking%2Ftracking-express.html%3Fsubmit%3D1%26tracking-id%3D%22%2Btracking%3B%0Awindow.open%28url%2C%20%27_blank%27%29%3B%0Avoid%280%29%3B
+// javascript:tracking%20%3D%20prompt%28%22Enter%20DHL%20tracking%20number%22%29%3B%0Aurl%20%3D%20%22https%3A%2F%2Fwww.dhl.com%2Fus-en%2Fhome%2Ftracking%2Ftracking-express.html%3Fsubmit%3D1%26tracking-id%3D%22%2Btracking%3B%0Awindow.open%28url%2C%20%27_blank%27%29%3B%0Avoid%280%29%3B
+
+
+
+javascript:tracking = prompt("Enter DHL tracking number");
+url = "https://www.dhl.com/us-en/home/tracking/tracking-express.html?submit=1&tracking-id="+tracking;
+window.open(url, '_blank');
+void(0);
