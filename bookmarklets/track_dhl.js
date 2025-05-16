7962705865c2dc5ad938beba254cddbd43be473d
erhannis <eyeillus@gmail.com>
2025-05-16 12:06:08 -0400

    Added a bunch of bookmarklets

---
+++ bookmarklets/track_dhl.js
@@ -0,0 +1,1 @@
+javascript:tracking%20%3D%20prompt%28%22Enter%20DHL%20tracking%20number%22%29%3B%0Aurl%20%3D%20%22https%3A%2F%2Fwww.dhl.com%2Fus-en%2Fhome%2Ftracking%2Ftracking-express.html%3Fsubmit%3D1%26tracking-id%3D%22%2Btracking%3B%0Awindow.open%28url%2C%20%27_blank%27%29%3B%0Avoid%280%29%3B
